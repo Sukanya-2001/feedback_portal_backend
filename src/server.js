9e7a67aebb252f9988af2458a58fd5edacc28ee4
Sukanya <sukanya.sett@webskitters.in>
2026-06-23 00:12:52 +0530

    change cors

--- src/server.js
+++ src/server.js
@@ -14,10 +14,12 @@ connectRedis();
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
